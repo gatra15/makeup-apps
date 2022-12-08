a01ef9d15a651bf4940bf4558e13f425df3609c3
daniel sibarani <64211478+peachguri@users.noreply.github.com>
2022-12-08 22:13:56 +0700

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {setSearch, search} from "react";
+
+const Footer=()=>{
+    return(
+        <>
+            <div className="footer">
+                <h6 style={{ margin: 20 }}>Kelompok 18 Praktikum RPLBK 2022</h6>
+            </div>
+        </>
+    )
+}
+
+export default Footer;
